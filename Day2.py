74cad5437cc3460e8680fa71b4b16bd7ddae0b9f
Dhee <150270603+heydhee@users.noreply.github.com>
2024-04-21 18:42:20 +0530

    Update Day2.py

--- Day2.py
+++ Day2.py
@@ -1,3 +1,8 @@
+# Day 2
+# Mathematical operators
+# PEDMAS
+# Number Manipulation and Fstrings
+
 print("Welcome to Tip Calculator")
 bill=float(input("Enter the bill amount"))
 interest=int(input("What percentage of tip would you like to give? 10, 12 or 15? "))
