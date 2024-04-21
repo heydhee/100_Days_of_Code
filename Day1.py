15f7a946f9231e53e29b07da7805c8f3edd031c3
Dhee <150270603+heydhee@users.noreply.github.com>
2024-04-21 18:37:37 +0530

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -1,3 +1,7 @@
+#Day 1
+#Variables
+#Data types
+#Basic functions - Input and Output
 print("Welcome to Dhee Band name generator!!!")
 city=input("Which city did you grow up in?")
 pet=input("What is the name of your pet?")
